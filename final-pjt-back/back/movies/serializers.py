98b705d5612fdecae10f5229887b729cccac9c52
김성현 <7h1320@naver.com>
2023-05-19 11:33:32 +0900

    api update genre & top100 genre filter

--- final-pjt-back/back/movies/serializers.py
+++ final-pjt-back/back/movies/serializers.py
@@ -5,7 +5,7 @@ from .models import *
 class GenreSerializer(serializers.ModelSerializer):
     class Meta():
         model = Genre
-        fields = ('name',)
+        fields = ('name', 'id')
 
 
 ############################################
