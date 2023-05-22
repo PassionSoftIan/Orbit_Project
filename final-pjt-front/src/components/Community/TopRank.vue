37a8113af0f2cf7eeff767977631f34dddb35bf5
swcl309 <swcl309@naver.com>
2023-05-23 02:26:55 +0900

    20230523 0226

--- final-pjt-front/src/components/Community/TopRank.vue
+++ final-pjt-front/src/components/Community/TopRank.vue
@@ -1,20 +1,37 @@
 <template>
-<div class="box">
-  <a
-  v-for='Movie in this.Movies.data'
-  :key="Movie.id"
-    href="">
-    <router-link :to="{name:'MovieDetail', params:{moviepk:Movie.id}}">
-    <img :src="`https://image.tmdb.org/t/p/original/`+Movie.poster_path" alt="">
-    </router-link>
-    </a>
-</div>
+  <carousel
+    class="info-carousel"
+    :per-page="7"
+    :paginationEnabled="false"
+    :navigation-enabled="true"
+    navigationNextLabel=">"
+    navigationPrevLabel="<">
+    <slide v-for="Movie in this.Movies.data"
+    :key="Movie.id"
+    data-name="MySlideName"
+    @slideclick="handleSlideClick">
+     <router-link :to="{name:'MovieDetail', params:{moviepk:Movie.id}}">
+      <img :src="`https://image.tmdb.org/t/p/original/`+Movie.poster_path" alt="">
+      </router-link>
+    </slide>
+  </carousel>
+
 </template>
 
-<script>
 
+<script>
+import { Carousel, Slide } from 'vue-carousel';
 export default {
   name:"TopRank",
+    components: {
+    Carousel,
+    Slide
+  },
+  methods:{
+      handleSlideClick (dataset){
+    console.log(dataset.index, dataset.name)
+  }
+  },
 
 
   props:{
@@ -28,28 +45,34 @@ export default {
 <style scoped>
 img {
   max-width: 97%;
-  
   width: 97%;
   height: 97%;
+  overflow: visible;
 }
-  .box {
+  .info-carousel {
+    /* padding: 2px; */
+    margin-block: 10px;
     display: grid;
     grid-gap: 5px;
-    grid-template-columns: repeat(6, minmax(100px, 1fr));
+
   }
   
-  .box a {
+  .info-carousel img {
     transition: transform .3s;  
+    
   }
   
-  .box a:hover {
+  .info-carousel img:hover {
     transition: transform .3s;
     -ms-transform: scale(1.4);
     -webkit-transform: scale(1.4);  
     transform: scale(1.4);
+    
   }
   
-  .box img {
+  .info-carousel img {
     border-radius: 2px;
+    
   }
 </style>
+
