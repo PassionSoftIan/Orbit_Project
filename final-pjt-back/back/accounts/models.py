54d25f8de173b57b23823c04e99786776292a838
Glaxy Book Ian <passionsoft.ian@gmail.com>
2023-05-26 04:22:01 +0900

    coin default = 10 |230526

--- final-pjt-back/back/accounts/models.py
+++ final-pjt-back/back/accounts/models.py
@@ -7,4 +7,4 @@ class User(AbstractUser):
     nick_name = models.CharField(blank=True, max_length=10)
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
     like_reviews = models.ManyToManyField(Review, related_name='liked_user')
-    coins = models.IntegerField(default = 0)
+    coins = models.IntegerField(default = 10)
